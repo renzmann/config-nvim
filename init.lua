c601ef017b587167b0c9d48ba3c83cedb9a9a34f
Robb Enzmann <EnzmannR@aetna.com>
2022-10-03 23:23:44 -0400

    add relative filepath to lualine section

--- init.lua
+++ init.lua
@@ -169,6 +169,20 @@ require("lualine").setup({
       component_separators = { left = "", "" },
       section_separators = { left = "", right = "" },
    },
+   sections = {
+      lualine_a = {'mode'},
+      lualine_b = {'branch', 'diff', 'diagnostics'},
+      lualine_c = {
+         {
+            'filename',
+            file_status = true, -- displays file status (readonly status, modified status)
+            path = 1, -- 0 = just filename, 1 = relative path, 2 = absolute path
+         }
+      },
+      lualine_x = {'encoding', 'fileformat', 'filetype'},
+      lualine_y = {'progress'},
+      lualine_z = {'location'}
+   },
 })
 -- }}}
 
