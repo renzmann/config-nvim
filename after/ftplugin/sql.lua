f6e3ee22d768da1fe8b32d9bb13e70a4d2a6d5ff
Robb Enzmann <EnzmannR@aetna.com>
2022-12-12 10:40:07 -0500

    four spaces for SQL

--- after/ftplugin/sql.lua
+++ after/ftplugin/sql.lua
@@ -1,6 +1,6 @@
-vim.bo.tabstop = 3
-vim.bo.softtabstop = 3
-vim.bo.shiftwidth = 3
+vim.bo.tabstop = 4
+vim.bo.softtabstop = 4
+vim.bo.shiftwidth = 4
 vim.bo.textwidth = 120
 vim.bo.expandtab = true
 
